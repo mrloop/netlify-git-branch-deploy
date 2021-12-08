3a065da24e94d72053ca1295b94c37e637d20fd5
mrloop <ewan@mrloop.com>
2021-12-08 21:35:12 +0000

    Not quiet revert "If tests timeout try and cleanup test deploy"
    This reverts the intention of "ac2d03a0d95e0966b9626c8258ee50e3f4d0be57"

--- test/commands/index.test.ts
+++ test/commands/index.test.ts
@@ -1,18 +1,10 @@
 import {expect, test} from '@oclif/test'
 import * as crypto from 'crypto'
-import Site from '../../src/site'
 
 const str = crypto.randomBytes(2).toString('hex')
 const name = `test-${str}`
 
 describe('commands', () => {
-  after(async function () {
-    // if 'delete' fails try deleting again here
-    return new Site(name, (msg, ...args) => {
-      console.log(`(after hook) ${msg}`, ...args)
-    }).delete()
-  })
-
   describe('deploy', () => {
     test
     .stderr({print: true})
